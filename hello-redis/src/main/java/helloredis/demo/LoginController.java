4163f47ba054530caa173c12ff439c3d0aa7782b
wisehero10 <godjw96@gmail.com>
2023-02-07 01:46:07 +0900

    Redis를 이용한 세션 클러스터링

---
+++ hello-redis/src/main/java/helloredis/demo/LoginController.java
@@ -0,0 +1,23 @@
+package helloredis.demo;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+	@GetMapping("/login")
+	public String login(HttpSession session, @RequestParam String name) {
+		session.setAttribute("name", name);
+
+		return "saved";
+	}
+
+	@GetMapping("/myName")
+	public String myName(HttpSession session) {
+		return (String)session.getAttribute(session.getId());
+	}
+}
